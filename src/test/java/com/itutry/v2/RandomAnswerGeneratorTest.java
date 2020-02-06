882e1d2c709897b95f71fbe49b33350d3b50a300
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-02-06 17:14:05 +0800

    v2: add should_return_not_duplicate_numbers_when_generator_answer test case

--- src/test/java/com/itutry/v2/RandomAnswerGeneratorTest.java
+++ src/test/java/com/itutry/v2/RandomAnswerGeneratorTest.java
@@ -1,5 +1,7 @@
 package com.itutry.v2;
 
+import java.util.Arrays;
+import java.util.Set;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +17,17 @@ class RandomAnswerGeneratorTest {
     for (char num : answer.toCharArray()) {
       Assert.assertTrue(Character.isDigit(num));
     }
+  }
+
+  @Test
+  void should_return_not_duplicate_numbers_when_generator_answer() {
+    AnswerGenerator answerGenerator = new RandomAnswerGenerator();
+
+    String answer = answerGenerator.generate();
+
+    long notDuplicateCount = Arrays.stream(answer.split(""))
+        .distinct()
+        .count();
+    Assert.assertEquals(answer.length(), notDuplicateCount);
   }
 }
