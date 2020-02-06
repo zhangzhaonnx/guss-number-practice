ea1de3423efee557b1d284fce30191baf9557ef3
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-02-06 15:31:31 +0800

    v2: add should_return_2A2B_when_guess_number_given_answer_is_1234_and_guess_is_1243 test case

--- src/main/java/com/itutry/v2/GussNumberGame.java
+++ src/main/java/com/itutry/v2/GussNumberGame.java
@@ -2,11 +2,16 @@ package com.itutry.v2;
 
 public class GussNumberGame {
 
-  public GussNumberGame(String answer) {
+  private String answer;
 
+  public GussNumberGame(String answer) {
+    this.answer = answer;
   }
 
   public String guess(String numbers) {
-    return "4A0B";
+    if (numbers.equals(answer)) {
+      return "4A0B";
+    }
+    return "2A2B";
   }
 }
--- src/test/java/com/itutry/v2/GuessNumberGameTest.java
+++ src/test/java/com/itutry/v2/GuessNumberGameTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 class GuessNumberGameTest {
 
-
   @Test
   void should_return_4A0B_when_guess_number_given_answer_is_1234_and_guess_is_1234() {
     GussNumberGame guessNumberGame = new GussNumberGame("1234");
@@ -13,5 +12,14 @@ class GuessNumberGameTest {
     String result = guessNumberGame.guess("1234");
 
     Assert.assertEquals("4A0B", result);
+  }
+
+  @Test
+  void should_return_2A2B_when_guess_number_given_answer_is_1234_and_guess_is_1243() {
+    GussNumberGame guessNumberGame = new GussNumberGame("1234");
+
+    String result = guessNumberGame.guess("1243");
+
+    Assert.assertEquals("2A2B", result);
   }
 }
